cc470b0fc961e9b8ce2ce10e3762611a1bf83320
yadach <47210083+yadach@users.noreply.github.com>
2022-11-17 16:53:33 +0900

    dump json

--- pydtk/bin/oas.py
+++ pydtk/bin/oas.py
@@ -2,37 +2,52 @@
 
 """Dump OpenAPI Specification."""
 
-import fire
+import glob
 import logging
 import os
+
+import fire
+
 import pydtk
-from pydtk.db.schemas import BaseSchema
-
-from pydtk.db.schemas import get_schema
-
-import glob
-from pydantic import BaseModel
+from pydtk.db.schemas import BaseSchema, get_schema
 
 
 class OpenAPISpecification(object):
+    """Generate OpenAPI Specification."""
+
     def __init__(self, out_dir: str) -> None:
+        assert os.path.isdir(out_dir), f"Directory '{out_dir}' is not found."
         self.out_dir = out_dir
         self._schema_dir = os.path.join(os.path.dirname(pydtk.__file__), "db", "schemas")
         pass
 
-    def dump(self):
+    def dump(self) -> None:
+        """Dump OpenAPI Specification."""
         api_versions = self.get_api_versions()
         schemas = []
         for api_ver in api_versions:
             schemas.extend(self.get_schemas(api_ver))
-        # for schema in schemas:
-        #     self.dump_schema_as_oas(schema)
+        for schema in schemas:
+            self.dump_schema_as_oas(schema, self.out_dir)
 
-    def get_api_versions(self):
+    def get_api_versions(self) -> list:
+        """Get all API versions.
+
+        Returns:
+            list: List of API versions.
+        """
         versions = [os.path.dirname(directory).replace(f"{self._schema_dir}/", "") for directory in glob.glob(f"{self._schema_dir}/*/*/", recursive=False)]
         return versions
 
-    def get_schemas(self, api_version: str):
+    def get_schemas(self, api_version: str) -> BaseSchema:
+        """Get all schemas.
+
+        Args:
+            api_version (str): Target schema's API version.
+
+        Returns:
+            BaseSchema (pydtk.db.schema.BaseSchema): All schema of the target API version.
+        """
         schema_dir = os.path.join(self._schema_dir, api_version.replace("/", os.sep).lower())
         schema_files = [file for file in glob.glob(f"{schema_dir}/*.py", recursive=False) if os.path.basename(file) != "__init__.py"]
         schemas = []
@@ -41,23 +56,37 @@ class OpenAPISpecification(object):
             schemas.append(get_schema(api_version=api_version, kind=kind))
         return schemas
 
+    def dump_schema_as_oas(self, schema: BaseSchema, output_dir: str) -> None:
+        """Dump schema information in the form of OpenAPI Specification format.
 
-    def dump_schema_as_oas(self, schema: BaseSchema, output_dir):
-        dump_dir = os.path.join(output_dir, schema._api_verson)
+        Args:
+            schema (BaseSchema): Target schema.
+            output_dir (str): Output directory.
+        """
+        dump_dir = os.path.join(output_dir, schema._api_version)
         os.makedirs(dump_dir, exist_ok=True)
         json_path = os.path.join(dump_dir, f"{schema._kind.lower()}.json")
-        oas = self._shcema_to_oas(schema)
+        oas = self._schema_to_oas(schema)
         with open(json_path, "w") as f:
             f.write(oas)
 
     def _schema_to_oas(self, schema: BaseSchema):
-        return schema._api_version
+        return schema.schema_json(indent=2)
 
 
-def dump_oas():
-    out_dir = "./tmp"
+def dump_oas(out_dir: str) -> None:
+    """Dump schema information in the form of OpenAPI Specification format.
+
+    Args:
+        out_dir (str): Output directory.
+    """
     oas = OpenAPISpecification(out_dir=out_dir)
     oas.dump()
+
+
+def script() -> None:
+    """Function for tool.poetry.scripts."""
+    fire.Fire(dump_oas)
 
 
 if __name__ == "__main__":
@@ -66,4 +95,4 @@ if __name__ == "__main__":
         level=logging.INFO,
         format="%(asctime)s (%(module)s:%(lineno)d) %(levelname)s: %(message)s",
     )
-    dump_oas()
+    fire.Fire(dump_oas)
