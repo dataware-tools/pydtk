98bb0ab80c10e976d16c96440cdde399c4a99c84
yadach <47210083+yadach@users.noreply.github.com>
2022-11-18 14:26:28 +0900

    ci test

--- pydtk/db/schemas/dataware-tools.com/v1alpha1/file.py
+++ pydtk/db/schemas/dataware-tools.com/v1alpha1/file.py
@@ -1,14 +1,16 @@
 from typing import Optional
+
+from pydantic import Field, constr
+
 from pydtk.db.schemas import BaseSchema
-from pydantic import constr, Field
 
 
 class File(BaseSchema):
     """Schema for files."""
 
     _api_version = 'dataware-tools.com/v1alpha1'
-    _kind = 'File'
+    _kind = 'Files-test'
     description: Optional[constr()] = Field(None, description="")
-    record_id: constr(min_length=1) = Field(..., description="")
+    record_id_test: constr(min_length=1) = Field(..., description="")
     path: constr(min_length=1) = Field(..., description="")
     contents: Optional[dict] = Field(None, description="")
