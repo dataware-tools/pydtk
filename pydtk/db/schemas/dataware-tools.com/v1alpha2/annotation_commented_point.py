48ca0946aa78a931c83a212a551ccb1baa45cb06
WatanabeToshimitsu <toshimitsu.watanabe@hdwlab.co.jp>
2023-01-04 14:07:21 +0900

    make timestamp nullable

---
+++ pydtk/db/schemas/dataware-tools.com/v1alpha2/annotation_commented_point.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel, Field, constr
+
+from pydtk.db.schemas import register_schema
+
+try:
+    # NOTE(kan-bayashi): absolute and relative path import does not work
+    from annotation import Annotation
+except ImportError:
+    import os
+    import sys
+
+    sys.path.append(os.path.dirname(__file__))
+    from annotation import Annotation
+
+
+class Point(BaseModel):
+    """Schema for point."""
+
+    x: float
+    y: float
+    z: float
+
+
+class CommentedPoint(BaseModel):
+    """Schema for commented point."""
+
+    text: constr(min_length=1) = Field(..., description="")
+    frame_id: constr(min_length=1) = Field(..., description="Cordinate ID.")
+    point: Point
+
+
+@register_schema
+class AnnotationCommentedPoint(Annotation):
+    """Schema for commented point annotation."""
+
+    _api_version = "dataware-tools.com/v1alpha1"
+    _kind = "AnnotationCommentedPoint"
+    commented_point: CommentedPoint
