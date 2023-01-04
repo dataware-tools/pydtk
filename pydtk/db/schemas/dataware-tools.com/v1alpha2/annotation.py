20f1b7f96c19b087629bc74cab862eda3b56c9a4
WatanabeToshimitsu <toshimitsu.watanabe@hdwlab.co.jp>
2023-01-04 16:54:43 +0900

    Add workaround for making properties nullable

--- pydtk/db/schemas/dataware-tools.com/v1alpha2/annotation.py
+++ pydtk/db/schemas/dataware-tools.com/v1alpha2/annotation.py
@@ -1,5 +1,5 @@
 from pydantic import Field, constr
-from typing import Optional
+from typing import Union
 
 from pydtk.db.schemas import BaseSchema, register_schema
 
@@ -13,7 +13,27 @@ class Annotation(BaseSchema):
     annotation_id: constr(min_length=1) = Field(..., description="")
     generation: int
     record_id: constr(min_length=1) = Field(..., description="")
-    timestamp_from: Optional[float]
-    timestamp_to: Optional[float]
+    timestamp_from: Union[float, None]
+    timestamp_to: Union[float, None]
     # TODO(watanabe): String?
     created_at: str
+
+    # Note(WatanabeToshimitsu): workaround for make properties nullable
+    # See: https://github.com/pydantic/pydantic/issues/1270#issuecomment-734454493
+    # This issue will be resolved in v2, See: https://docs.pydantic.dev/blog/pydantic-v2/#required-vs-nullable-cleanup
+    class Config:
+        @staticmethod
+        def schema_extra(schema, model):
+            for prop, value in schema.get("properties", {}).items():
+                # retrieve right field from alias or name
+                field = [x for x in model.__fields__.values() if x.alias == prop][0]
+                if field.allow_none:
+                    if "$ref" in value:
+                        if issubclass(field.type_, BaseSchema):
+                            # add 'title' in schema to have the exact same behaviour as the rest
+                            value["title"] = (
+                                field.type_.__config__.title or field.type_.__name__
+                            )
+                        value["anyOf"] = [{"$ref": value.pop("$ref")}]
+
+                    value["nullable"] = True
